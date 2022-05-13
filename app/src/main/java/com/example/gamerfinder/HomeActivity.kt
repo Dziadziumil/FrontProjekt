7d2478dd0d5ff80689f29d86a92aa3edb77b6021
columnn <piotr.piiatek@gmail.com>
2022-05-13 14:13:17 +0200

    migrated login to navigation component (fragments)

---
+++ app/src/main/java/com/example/gamerfinder/HomeActivity.kt
@@ -0,0 +1,27 @@
+package com.example.gamerfinder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.example.gamerfinder.databinding.ActivityHomeBinding
+
+class HomeActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.home_nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+}
