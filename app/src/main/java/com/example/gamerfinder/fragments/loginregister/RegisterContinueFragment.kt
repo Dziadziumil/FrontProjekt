ce8be484542d5cd937d2b80bd705f016c4d81045
PSyno <psynowiecki2000@gmail.com>
2022-06-08 19:40:59 +0200

    Alert message on register continue screen

--- app/src/main/java/com/example/gamerfinder/fragments/loginregister/RegisterContinueFragment.kt
+++ app/src/main/java/com/example/gamerfinder/fragments/loginregister/RegisterContinueFragment.kt
@@ -10,6 +10,7 @@ import android.view.ViewGroup
 import androidx.navigation.findNavController
 import com.example.gamerfinder.databinding.FragmentRegisterContinueBinding
 import com.example.gamerfinder.utils.*
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import java.util.*
 
 class RegisterContinueFragment : Fragment() {
@@ -39,21 +40,21 @@ class RegisterContinueFragment : Fragment() {
 
     private fun checkRegisterData(view: View) {
         var isCorrect = true
+        var isCorrectSerious = true
         if (binding.personName.length() < 2 || !namePattern.containsMatchIn(binding.personName.text.toString())) {
-            binding.personNameLayout.error = "Wrong user's Name."
+            binding.personNameLayout.error = "User's name is too short."
             isCorrect = false
         }
         if (binding.personSurname.length() < 2 || !namePattern.containsMatchIn(binding.personSurname.text.toString())) {
-            binding.personSurnameLayout.error = "Wrong user's Surname."
+            binding.personSurnameLayout.error = "User's surname is too short."
             isCorrect = false
         }
-        if (cYear - binding.personBirth.year < 13) {
-            binding.DateLayout.error = "You must be at least 13 years old."
-            isCorrect = false
+        if (cYear - binding.personBirth.year > 118) {
+            binding.DateLayout.error = "You can't be that old."
+            isCorrectSerious = false
         }
         var gender: String? = null
         if (!binding.buttonFemale.isChecked && !binding.buttonMale.isChecked && !binding.buttonOther.isChecked) {
-            binding.GenderLayout.error = "Please tell us your gender."
             isCorrect = false
         } else {
             when {
@@ -68,40 +69,84 @@ class RegisterContinueFragment : Fragment() {
                 }
             }
         }
-        if (isCorrect) {
-            val handler = Handler(Looper.getMainLooper())
-            HttpPut.UpdateUser.apply {
-                this.addListener(HttpListener(object : Action<Nothing> {
-                    override fun onMessage(
-                        isSuccess: Boolean,
-                        value: Nothing?
-                    ) {
-                        if (isSuccess) {
-                            handler.post {
-                                val actionNo =
-                                    RegisterContinueFragmentDirections.actionRegisterContinueFragmentToLoginFragment()
-                                view.findNavController().navigate(actionNo)
+        if (isCorrectSerious) {
+            if (isCorrect) {
+                val handler = Handler(Looper.getMainLooper())
+                HttpPut.UpdateUser.apply {
+                    this.addListener(HttpListener(object : Action<Nothing> {
+                        override fun onMessage(
+                            isSuccess: Boolean,
+                            value: Nothing?
+                        ) {
+                            if (isSuccess) {
+                                handler.post {
+                                    val actionNo =
+                                        RegisterContinueFragmentDirections.actionRegisterContinueFragmentToLoginFragment()
+                                    view.findNavController().navigate(actionNo)
+                                }
+                            } else {
+                                println("register wasn't a success!")
                             }
-                        } else {
-                            println("register wasn't a success!")
                         }
+                    }))
+                }.requestPost(
+                    RequestModels.UserData(
+                        AccountService(requireContext()).getCurrentUserId().toInt(),
+                        null,
+                        binding.personName.text.toString(),
+                        binding.personSurname.text.toString(),
+                        "${
+                            binding.personBirth.year.toString().padStart(4, '0')
+                        }-${
+                            binding.personBirth.month.toString().padStart(2, '0')
+                        }-${binding.personBirth.dayOfMonth.toString().padStart(2, '0')}T00:00:00",
+                        gender
+                    ),
+                    requireContext()
+                )
+            }
+            else {
+                MaterialAlertDialogBuilder(requireActivity())
+                    .setTitle("Alert")
+                    .setMessage("Not all fields have been filled in, are you sure you want to continue?")
+                    .setNegativeButton("No") { _, _ ->}
+                    .setPositiveButton("Yes") {dialog, which ->
+                        val handler = Handler(Looper.getMainLooper())
+                        HttpPut.UpdateUser.apply {
+                            this.addListener(HttpListener(object : Action<Nothing> {
+                                override fun onMessage(
+                                    isSuccess: Boolean,
+                                    value: Nothing?
+                                ) {
+                                    if (isSuccess) {
+                                        handler.post {
+                                            val actionNo =
+                                                RegisterContinueFragmentDirections.actionRegisterContinueFragmentToLoginFragment()
+                                            view.findNavController().navigate(actionNo)
+                                        }
+                                    } else {
+                                        println("register wasn't a success!")
+                                    }
+                                }
+                            }))
+                        }.requestPost(
+                            RequestModels.UserData(
+                                AccountService(requireContext()).getCurrentUserId().toInt(),
+                                null,
+                                binding.personName.text.toString(),
+                                binding.personSurname.text.toString(),
+                                "${
+                                    binding.personBirth.year.toString().padStart(4, '0')
+                                }-${
+                                    binding.personBirth.month.toString().padStart(2, '0')
+                                }-${binding.personBirth.dayOfMonth.toString().padStart(2, '0')}T00:00:00",
+                                gender
+                            ),
+                            requireContext()
+                        )
                     }
-                }))
-            }.requestPost(
-                RequestModels.UserData(
-                    AccountService(requireContext()).getCurrentUserId().toInt(),
-                    null,
-                    binding.personName.text.toString(),
-                    binding.personSurname.text.toString(),
-                    "${
-                        binding.personBirth.year.toString().padStart(4, '0')
-                    }-${
-                        binding.personBirth.month.toString().padStart(2, '0')
-                    }-${binding.personBirth.dayOfMonth.toString().padStart(2, '0')}T00:00:00",
-                    gender
-                ),
-                requireContext()
-            )
+                    .show()
+            }
         }
     }
 
